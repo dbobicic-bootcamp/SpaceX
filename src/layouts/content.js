bd0e827cca071e8ef66de81c1e303bc3f5a77f35
warrior97 <dbobicic@gmail.com>
2019-11-29 13:33:00 +0100

    Dodati filteri

--- src/layouts/content.js
+++ src/layouts/content.js
@@ -13,6 +13,11 @@ class Content {
             this.selectYear.appendChild(option);
         }
 
+        this.btnAsc = document.createElement('button');
+        this.btnAsc.textContent = 'Rastuce';
+
+        this.btnDsc = document.createElement('button');
+        this.btnDsc.textContent = 'Opadajuce';
 
         this.launches = new Launches();
 
@@ -20,17 +25,46 @@ class Content {
             //povucemo podatke s beka
             getAllLaunches().then(data => {
                 //filtriramo podatke po godini
-                data=data.filter(x=>x.links.mission_patch!=null && x.launch_year==this.selectYear.value)
+                data = data.filter(x => x.links.mission_patch != null && x.launch_year == this.selectYear.value)
                 //posaljemo u this.launches podatke koje zelimo da prikazemo
                 this.launches.loadData(data);
             })
         })
+        this.btnDsc.addEventListener('click',()=> {
+            //povucemo podatke s beka
+            getAllLaunches().then(data => {
+                //sortiramo podatke po godini
+                data = data.filter(x => x.links.mission_patch != null)
+                data = data.sort((a, b) => b.launch_year - a.launch_year);
+                //posaljemo u this.launches podatke koje zelimo da prikazemo
+
+                this.launches.loadData(data);
+            })
+
+
+        })
+        this.btnAsc.addEventListener('click',()=> {
+            //povucemo podatke s beka
+            getAllLaunches().then(data => {
+                //sortiramo podatke po godini
+                data = data.filter(x => x.links.mission_patch != null)
+                data = data.sort((a, b) => a.launch_year - b.launch_year);
+                //posaljemo u this.launches podatke koje zelimo da prikazemo
+                this.launches.loadData(data);
+            })
+
+
+        })
         this.node.appendChild(this.selectYear);
+        this.node.appendChild(this.btnAsc);
+        this.node.appendChild(this.btnDsc);
         this.node.appendChild(this.launches.getNode());
+
     }
     getNode() {
         return this.node;
     }
+    
 }
 export {
     Content
